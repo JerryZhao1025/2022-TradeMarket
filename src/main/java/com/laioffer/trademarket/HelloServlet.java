48b3aaf777872656a08e4b33737310ecb3627982
Jinyang (Jerry) Zhao <96359869+JerryZhao1025@users.noreply.github.com>
2022-08-17 16:35:47 -0400

    testing feature001

--- src/main/java/com/laioffer/trademarket/HelloServlet.java
+++ src/main/java/com/laioffer/trademarket/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello feature 001!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
