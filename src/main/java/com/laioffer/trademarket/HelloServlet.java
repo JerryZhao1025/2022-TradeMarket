ecdf701d0ba0186c79ea1f6a11e571100a239b72
wangyiqunumich <57782606+wangyiqunumich@users.noreply.github.com>
2022-08-17 16:16:16 -0500

    Feature002 (#5)
    * Test commit from Yiqun
    
    * Feature002
    
    * Add another message
    
    Ref: feature-002

--- src/main/java/com/laioffer/trademarket/HelloServlet.java
+++ src/main/java/com/laioffer/trademarket/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello feature 001!";
+        message = "Hello feature 002!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -18,6 +18,7 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
