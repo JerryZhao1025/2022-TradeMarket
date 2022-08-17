3634c04b51f40529ab3ecbc9d20a11a2408e7e65
wangyiqunumich <57782606+wangyiqunumich@users.noreply.github.com>
2022-08-17 16:50:27 -0500

    Feature/004 (#7)
    * Weird things happened.
    
    Ref: feature-004
    
    Co-authored-by: xincheng <61475872+xincheng121@users.noreply.github.com>

--- src/main/java/com/laioffer/trademarket/HelloServlet.java
+++ src/main/java/com/laioffer/trademarket/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello feature 003.2!!";
+        message = "Hello feature 004!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
