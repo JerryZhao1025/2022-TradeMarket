e27e69393fc0d6f8adbdf0c7b254e02994fcbbdb
yiqun <wyq@umich.edu>
2022-08-24 14:55:40 -0500

    Created the fields of Order under Entity. Modified Post as well to pass compiler check. Yiqun(Huaiting) 20220824

--- src/main/java/com/laioffer/trademarket/entity/Order.java
+++ src/main/java/com/laioffer/trademarket/entity/Order.java
@@ -1,4 +1,34 @@
 package com.laioffer.trademarket.entity;
 
-public class Order {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "order")
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 4293887464938867671L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(unique = true)
+    private Post post;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date createTimeStamp;
+
+    private boolean paid;
+
+    private boolean shipped;
+
+
 }
--- src/main/java/com/laioffer/trademarket/entity/Post.java
+++ src/main/java/com/laioffer/trademarket/entity/Post.java
@@ -1,4 +1,11 @@
 package com.laioffer.trademarket.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 }
