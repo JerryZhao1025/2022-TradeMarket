cca169f35361fcdcdbf2e0c74d9c9957fb7c8d24
Jinyang (Jerry) Zhao <96359869+JerryZhao1025@users.noreply.github.com>
2022-08-23 23:55:06 -0400

    create empty files and init user entity class (#12)

---
+++ src/main/java/com/laioffer/trademarket/dao/OrderDao.java
@@ -0,0 +1,4 @@
+package com.laioffer.trademarket.dao;
+
+public class OrderDao {
+}
---
+++ src/main/java/com/laioffer/trademarket/entity/User.java
@@ -0,0 +1,90 @@
+package com.laioffer.trademarket.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 2652327633296064143L;
+
+    @Id
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private boolean enabled;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    private List<Post> postList;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    private List<Post> myCart;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    private List<Post> favoriteList;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Post> getPostList() {
+        return postList;
+    }
+
+    public void setPostList(List<Post> postList) {
+        this.postList = postList;
+    }
+
+    public List<Post> getMyCart() {
+        return myCart;
+    }
+
+    public void setMyCart(List<Post> myCart) {
+        this.myCart = myCart;
+    }
+
+    public List<Post> getFavoriteList() {
+        return favoriteList;
+    }
+
+    public void setFavoriteList(List<Post> favoriteList) {
+        this.favoriteList = favoriteList;
+    }
+}
