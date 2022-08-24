01d79399fd2514dd2c997a500ca21ee4e420997a
tingxu <tingxumarch@gmail.com>
2022-08-24 13:53:31 -0700

    post entity and image entity
    REF: feature/003-create_post_and_image_entity

---
+++ src/main/java/com/laioffer/trademarket/entity/Image.java
@@ -0,0 +1,48 @@
+package com.laioffer.trademarket.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "images")
+public class Image implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String url;
+
+    @ManyToOne
+    @JsonIgnore
+    private Post post;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
--- src/main/java/com/laioffer/trademarket/entity/Post.java
+++ src/main/java/com/laioffer/trademarket/entity/Post.java
@@ -1,4 +1,102 @@
 package com.laioffer.trademarket.entity;
 
-public class Post {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.List;
+
+
+@Entity
+@Table(name = "posts")
+public class Post implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private Timestamp date;
+
+    private boolean soldStatus;
+
+    private double price;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    private List<Image> images;
+
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public boolean getSoldStatus() {
+        return soldStatus;
+    }
+
+    public void setSoldStatus(boolean soldStatus) {
+        this.soldStatus = soldStatus;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User owner) {
+        this.user = owner;
+    }
+
 }
