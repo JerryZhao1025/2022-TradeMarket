f8c5873724a6fefa58195533a627d6100df4834c
yiqun <wyq@umich.edu>
2022-08-24 19:44:15 -0500

    Added getters and setters

--- src/main/java/com/laioffer/trademarket/entity/Order.java
+++ src/main/java/com/laioffer/trademarket/entity/Order.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "order")
@@ -30,5 +31,51 @@ public class Order implements Serializable {
 
     private boolean shipped;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Date getCreateTimeStamp() {
+        return createTimeStamp;
+    }
+
+    public void setCreateTimeStamp(Date createTimeStamp) {
+        this.createTimeStamp = createTimeStamp;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public boolean isShipped() {
+        return shipped;
+    }
+
+    public void setShipped(boolean shipped) {
+        this.shipped = shipped;
+    }
 }
