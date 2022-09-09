9297ed4ea7f348993ba5d92f8b67a63487218a17
Jinyang (Jerry) Zhao <96359869+JerryZhao1025@users.noreply.github.com>
2022-09-09 00:11:27 -0400

    init post develop branch

--- src/main/java/com/laioffer/tradeMarket/service/PostService.java
+++ src/main/java/com/laioffer/tradeMarket/service/PostService.java
@@ -1,5 +1,29 @@
 package com.laioffer.tradeMarket.service;
 
-public class PostService {
+import com.laioffer.tradeMarket.dao.PostDao;
+import com.laioffer.tradeMarket.entity.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class PostService {
+    private final PostDao postDao;
+
+    @Autowired
+    public PostService(PostDao postDao) {
+        this.postDao = postDao;
+    }
+
+    public void addPost(Post post) {
+        postDao.addPost(post);
+    }
+
+    public void editPost(int postID, Post newPost) {
+        postDao.editPost(postID, newPost);
+    }
+
+    public void deletePost(int postID) {
+        // 我认为这里还是需要用一下Authentication的方法进行验证，如果没有验证的话，就有可能所有人都可以通过一个组件随意删除网站上的post
+        postDao.deletePost(postID);
+    }
 }
