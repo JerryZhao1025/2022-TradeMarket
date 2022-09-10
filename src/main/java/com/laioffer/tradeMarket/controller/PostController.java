77134eda7e567f260e30667f215143bc6b5c4b3c
Jinyang (Jerry) Zhao <96359869+JerryZhao1025@users.noreply.github.com>
2022-09-09 21:29:44 -0400

    post controller init

--- src/main/java/com/laioffer/tradeMarket/controller/PostController.java
+++ src/main/java/com/laioffer/tradeMarket/controller/PostController.java
@@ -7,44 +7,47 @@ import com.laioffer.tradeMarket.service.PostService;
 import com.laioffer.tradeMarket.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Controller
 public class PostController {
     private final PostService postService;
 
     private TagService tagService;
+<<<<<<< HEAD
 
+=======
+>>>>>>> da52b3e (post controller init)
     @Autowired
-    public PostController(PostService postService) {
+    public PostController(PostService postService, TagService tagService) {
         this.postService = postService;
+        this.tagService = tagService;
     }
 
     @RequestMapping(value = {"/post/newPost"}, method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void addPost(@PathVariable("postID") int postID,
+    public void addPost(@RequestBody Post post,
                          HttpServletResponse response) {
-        // logic Tire
-//        postService.method(att1, attr2);
+        postService.addPost(post);
     }
-
     @RequestMapping(value = {"/post/{postID}/edit"}, method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public void editPost(@PathVariable("postID") int postID, @RequestBody Post post,
                          HttpServletResponse response) {
-        // logic Tire
-//        postService.method(att1, attr2);
+        postService.editPost(postID, post);
     }
 
     @RequestMapping(value = {"/post/{postID}/delete"}, method = RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.OK)
     public void deletePost(@PathVariable("postID") int postID, @RequestBody Post post,
                          HttpServletResponse response) {
-        // logic Tire
-//        postService.method(att1, attr2);
+        postService.deletePost(postID);
     }
 
+<<<<<<< HEAD
 
     @RequestMapping(value = {"/post/{postID}/addTag"}, method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.OK)
@@ -57,12 +60,28 @@ public class PostController {
     @ResponseStatus(value = HttpStatus.OK)
     public void removeTag(@PathVariable("postID") int tagId, int postId, @RequestBody Tag tag,
                        HttpServletResponse response) {
+=======
+    @RequestMapping(value = {"/post/{postID}/addTag/{tagID}"}, method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void addTag(@PathVariable("postID") int postId, @PathVariable("tagID") int tagId,
+                       HttpServletResponse response) {
+        tagService.addTag(tagId, postId);
+    }
+
+    @RequestMapping(value = {"/post/{postID}/removeTag/{tagID}"}, method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void removeTag(@PathVariable("postID") int postId, @PathVariable("tagID") int tagId,
+                          HttpServletResponse response) {
+>>>>>>> da52b3e (post controller init)
         tagService.removeTag(tagId, postId);
     }
 
     // =========================坚决不要动这下面的code，让做media的同学自己搞，不然可能会有冲突=============================
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> da52b3e (post controller init)
     @RequestMapping(value = {"/post/{postID}/addMedia"}, method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.OK)
     public void addMedia(@PathVariable("postID") int postID, @RequestBody Media media,
