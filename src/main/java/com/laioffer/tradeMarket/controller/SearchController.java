ce7f01d3da144010f4a7a5e69b82c2d0bef46e69
tingxu <tingxumarch@gmail.com>
2022-09-11 21:14:41 -0700

    Update TagDao, TagService. Add SearchController
    REF: FC-12

---
+++ src/main/java/com/laioffer/tradeMarket/controller/SearchController.java
@@ -0,0 +1,34 @@
+package com.laioffer.tradeMarket.controller;
+
+import com.laioffer.tradeMarket.entity.Post;
+import com.laioffer.tradeMarket.entity.Tag;
+import com.laioffer.tradeMarket.service.PostService;
+import com.laioffer.tradeMarket.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Set;
+
+@Controller
+public class SearchController {
+    private final PostService postService;
+    private final TagService tagService;
+
+    @Autowired
+    public SearchController(PostService postService, TagService tagService) {
+        this.postService = postService;
+        this.tagService = tagService;
+    }
+
+    @RequestMapping(value = {"/searchByTagId/{tagID}"}, method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+
+    public Set<Post> getPostsByTag(@PathVariable("tagID") int tagId, @RequestBody Tag tag, HttpServletResponse response){
+        return tagService.getAllPosts(tagId);
+
+    }
+}
