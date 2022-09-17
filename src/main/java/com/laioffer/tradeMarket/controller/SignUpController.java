425b94758def8d096b89a67b2f64c104de2d0e29
wangyiqunumich <57782606+wangyiqunumich@users.noreply.github.com>
2022-09-16 21:31:30 -0500

    Fc 7 sign up (#30)
    * Signup feature implemented.
        - Updated SignUpController
        - Updated UserService: signUp, get user by username and email
        - Updated UserDao: signUp, get user by username and email.
    
    * return token after successfully signup

--- src/main/java/com/laioffer/tradeMarket/controller/SignUpController.java
+++ src/main/java/com/laioffer/tradeMarket/controller/SignUpController.java
@@ -1,4 +1,52 @@
 package com.laioffer.tradeMarket.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.tradeMarket.entity.User;
+import com.laioffer.tradeMarket.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.crypto.SecretKey;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 public class SignUpController {
+    @Autowired
+    private UserService userService;
+
+
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public void signUp(@RequestBody User user, HttpServletResponse response) throws IOException {
+        try {
+            userService.signUp(user);
+            response.setStatus(HttpStatus.CREATED.value());
+
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("username", user.getUsername());
+            claims.put("password", user.getPassword());
+
+            Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+            String jws = Jwts.builder().setClaims(claims).signWith(key).compact();
+            response.getOutputStream().println(jws);
+        } catch (Exception exception) {
+            response.setStatus(HttpStatus.CONFLICT.value());
+            Map<String, Object> data = new HashMap<>();
+            data.put("message", exception.getMessage());
+            response.getOutputStream().println(objectMapper.writeValueAsString(data));
+        }
+
+    }
 }
