7abde752faf94eecf8a9b215d45584b3c39a0504
Oliver Feng <wenlongfeng8@gmail.com>
2022-09-17 23:28:59 -0400

    signin temp

--- src/main/java/com/laioffer/tradeMarket/controller/HelloController.java
+++ src/main/java/com/laioffer/tradeMarket/controller/HelloController.java
@@ -1,15 +1,26 @@
 package com.laioffer.tradeMarket.controller;
 
+import com.laioffer.tradeMarket.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import io.jsonwebtoken.Jwts;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HelloController {
-
+    @Autowired
+    private TokenService tokenService;
     @RequestMapping("hello")
     @ResponseBody
-    public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Hello "+name+"!!";
+    public String helloWorld(@RequestParam("username") String username, @RequestParam("token") String token,
+                             HttpServletResponse response) {
+
+        tokenService.verify(token, username);
+//            return "Hello "+username+"!!";
+//        }
+
+        return "not right token";
     }
 }
--- src/main/java/com/laioffer/tradeMarket/service/TokenService.java
+++ src/main/java/com/laioffer/tradeMarket/service/TokenService.java
@@ -1,24 +1,43 @@
 package com.laioffer.tradeMarket.service;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 import java.util.Date;
 
 @Service
 public class TokenService {
-
+    String FLAGKey = "This is our key of FLAGCAMP";
     public String getJWTToken(String username, String password) {
-        String token = Jwts
-                .builder()
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 600000))
-                .signWith(SignatureAlgorithm.HS512,
-                        password.getBytes()).compact();
+//        String token = Jwts.builder().setSubject(username)//.signWith(key).compact();
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+//                .signWith(SignatureAlgorithm.HS512,
+//                        FLAGKey)
+//                .compact();
+//        String token = Jwts
+//                .builder()
+//                .setSubject(username)
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+//                .signWith(SignatureAlgorithm.HS512,
+//                        FLAGKey).compact();
+        String token = Jwts.builder().setSubject("Joe").signWith(SignatureAlgorithm.HS512,  FLAGKey).compact();
 
-        return "Bearer " + token;
+        return token;
 
+    }
+
+    public void verify(String token, String username) {
+//        try {
+//            Jwts.parserBuilder().setSigningKey(FLAGKey).build().parseClaimsJws(token);
+//        } catch (JwtException e) {
+//
+//            //don't trust the JWT!
+//        }
     }
 }
