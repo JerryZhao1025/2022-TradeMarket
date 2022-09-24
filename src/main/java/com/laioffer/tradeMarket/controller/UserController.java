5974eb94def56c1ecbe3d07e5a5a0675af258488
Jinyang (Jerry) Zhao <96359869+JerryZhao1025@users.noreply.github.com>
2022-09-24 03:00:29 -0400

    move APIs around

--- src/main/java/com/laioffer/tradeMarket/controller/UserController.java
+++ src/main/java/com/laioffer/tradeMarket/controller/UserController.java
@@ -1,28 +1,86 @@
 package com.laioffer.tradeMarket.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laioffer.tradeMarket.entity.User;
-import com.laioffer.tradeMarket.service.PostService;
-import com.laioffer.tradeMarket.service.TagService;
-import com.laioffer.tradeMarket.service.TokenService;
-import com.laioffer.tradeMarket.service.UserService;
+import com.laioffer.tradeMarket.service.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserController {
     private final UserService userService;
-
     private final TokenService tokenService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
-    public UserController(PostService postService, TagService tagService, UserService userService, TokenService tokenService) {
+    public UserController(UserService userService, TokenService tokenService) {
         this.userService = userService;
         this.tokenService = tokenService;
+    }
+
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public void signIn(@RequestBody User user, HttpServletResponse response) throws IOException {
+        try {
+            User userInDB = userService.getUser(user.getUsername());
+            if (user.getPassword().equals(userInDB.getPassword())) {
+
+                response.setStatus(HttpStatus.CREATED.value());
+                String token = tokenService.getJWTToken(user.getUsername());
+                response.getOutputStream().println(token);
+            } else {
+                // wrong password
+                response.setStatus(HttpStatus.CONFLICT.value());
+                Map<String, Object> data = new HashMap<>();
+                data.put("message", "Wrong Password");
+                response.getOutputStream().println(objectMapper.writeValueAsString(data));
+            }
+
+        } catch (Exception exception) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            Map<String, Object> data = new HashMap<>();
+            data.put("message", exception.getMessage());
+            response.getOutputStream().println(objectMapper.writeValueAsString(data));
+        }
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public void signUp(@RequestBody User user, HttpServletResponse response) throws IOException {
+        try {
+            userService.signUp(user);
+            response.setStatus(HttpStatus.CREATED.value());
+
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("username", user.getUsername());
+            claims.put("password", user.getPassword());
+
+            Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+            String jws = Jwts.builder().setClaims(claims).signWith(key).compact(); // token
+
+            Map<String, Object> claim2 = Jwts.parser().setSigningKey(key).parseClaimsJws(jws).getBody();
+            System.out.println();
+            System.out.println(claim2.get("password"));
+
+            response.getOutputStream().println(jws);
+            response.getOutputStream().println((String)claim2.get("username"));
+            response.getOutputStream().println((String)claim2.get("password"));
+
+        } catch (Exception exception) {
+            response.setStatus(HttpStatus.CONFLICT.value());
+            Map<String, Object> data = new HashMap<>();
+            data.put("message", exception.getMessage());
+            response.getOutputStream().println(objectMapper.writeValueAsString(data));
+        }
     }
 
     @RequestMapping(value = {"/user/{username}"}, method = RequestMethod.GET)
