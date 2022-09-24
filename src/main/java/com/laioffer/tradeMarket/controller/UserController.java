c605d4ba5bd92673a80903e720b53bc2bf6ca7d0
Jinyang (Jerry) Zhao <96359869+JerryZhao1025@users.noreply.github.com>
2022-09-24 03:55:57 -0400

    add a place order API

--- src/main/java/com/laioffer/tradeMarket/controller/UserController.java
+++ src/main/java/com/laioffer/tradeMarket/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.laioffer.tradeMarket.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.tradeMarket.entity.Order;
+import com.laioffer.tradeMarket.entity.Post;
 import com.laioffer.tradeMarket.entity.User;
 import com.laioffer.tradeMarket.service.*;
 import io.jsonwebtoken.Jwts;
@@ -21,12 +23,19 @@ import java.util.Map;
 public class UserController {
     private final UserService userService;
     private final TokenService tokenService;
+    private final PostService postService;
+    private final OrderService orderService;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
-    public UserController(UserService userService, TokenService tokenService) {
+    public UserController(UserService userService,
+                          TokenService tokenService,
+                          PostService postService,
+                          OrderService orderService) {
         this.userService = userService;
         this.tokenService = tokenService;
+        this.postService = postService;
+        this.orderService = orderService;
     }
 
     @RequestMapping(value = "/signin", method = RequestMethod.POST)
@@ -55,25 +64,26 @@ public class UserController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
     public void signUp(@RequestBody User user, HttpServletResponse response) throws IOException {
         try {
             userService.signUp(user);
-            response.setStatus(HttpStatus.CREATED.value());
+//            response.setStatus(HttpStatus.CREATED.value());
 
-            Map<String, Object> claims = new HashMap<>();
-            claims.put("username", user.getUsername());
-            claims.put("password", user.getPassword());
-
-            Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-            String jws = Jwts.builder().setClaims(claims).signWith(key).compact(); // token
-
-            Map<String, Object> claim2 = Jwts.parser().setSigningKey(key).parseClaimsJws(jws).getBody();
-            System.out.println();
-            System.out.println(claim2.get("password"));
-
-            response.getOutputStream().println(jws);
-            response.getOutputStream().println((String)claim2.get("username"));
-            response.getOutputStream().println((String)claim2.get("password"));
+//            Map<String, Object> claims = new HashMap<>();
+//            claims.put("username", user.getUsername());
+//            claims.put("password", user.getPassword());
+//
+//            Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//            String jws = Jwts.builder().setClaims(claims).signWith(key).compact(); // token
+//
+//            Map<String, Object> claim2 = Jwts.parser().setSigningKey(key).parseClaimsJws(jws).getBody();
+//            System.out.println();
+//            System.out.println(claim2.get("password"));
+//
+//            response.getOutputStream().println(jws);
+//            response.getOutputStream().println((String)claim2.get("username"));
+//            response.getOutputStream().println((String)claim2.get("password"));
 
         } catch (Exception exception) {
             response.setStatus(HttpStatus.CONFLICT.value());
@@ -92,11 +102,26 @@ public class UserController {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return null;
         }
-
         if (tokenService.getUsernameFromToken(token).equals(username)){ //get all user info if token matches current user
             return userService.getUser(username);
         } else { // get part user info if it's a visitor
             return userService.getPartUserInfo(username);
         }
+    }
+
+    @RequestMapping(value = {"/newOrder/{postID}"}, method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @ResponseBody
+    public Order placeOrder(@PathVariable("postID") int postID, @RequestHeader("Authorization") String token,
+                            HttpServletResponse response){
+        if (!tokenService.verify(token)) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return null;
+        }
+        String username = tokenService.getUsernameFromToken(token);
+        User user = userService.getUser(username);
+        Post post = postService.getPost(postID);
+        orderService.newOrder(user, post);
+        return null;
     }
 }
